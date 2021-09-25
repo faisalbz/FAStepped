c73725d0d8346f723fec6d4c102c72b4906ecc72
Faisal AL-Otaibi <faysl4041@gmail.com>
2021-09-25 13:50:57 +0300

    Initial commit

--- Example/FAStepped/ViewController.swift
+++ Example/FAStepped/ViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import FAStepped
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var faSteppedView: FAStepped!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        faSteppedView.currentTab = Int(sender.value)
     }
 
 }
